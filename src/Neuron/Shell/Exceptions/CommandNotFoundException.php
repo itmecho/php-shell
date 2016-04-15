abe50ed1ec1b9fc58b4bd4aecd9026109d2a659b
Iain Earl <synapse791@gmail.com>
2016-04-15 09:41:38 +0100

    Altering exception message

--- src/Neuron/Shell/Exceptions/CommandNotFoundException.php
+++ src/Neuron/Shell/Exceptions/CommandNotFoundException.php
@@ -6,7 +6,7 @@ class CommandNotFoundException extends \Exception
 {
     public function __construct($cmd)
     {
-        $message = "Command not found: {$cmd}";
+        $message = "Command not found: '{$cmd}'. Make sure to pass the absolute path to the binary.";
         parent::__construct($message);
     }
 }
